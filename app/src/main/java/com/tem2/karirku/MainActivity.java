50068a1843d6fad37d62abf5c72d7e4d2394f763
DarisPutra <darisff2@gmail.com>
2025-10-02 13:50:58 +0700

    ganti nama

--- app/src/main/java/com/tem2/karirku/MainActivity.java
+++ app/src/main/java/com/tem2/karirku/MainActivity.java
@@ -18,7 +18,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_login);
 
         // Ambil view
         TextView daftarText = findViewById(R.id.textView5); // "Daftar"
